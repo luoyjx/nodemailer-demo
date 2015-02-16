9a69b4f32b85f860698f2b57b27092a40668da71
yanjixiong <yjk99@qq.com>
2015-02-16 15:01:13 +0800

    github地址

--- email.js
+++ email.js
@@ -15,7 +15,7 @@ var mailOptions = {
     to: '1111@xx.com,222@xx.com,333@xx.com', // list of receivers
     subject: '这是一封测试邮件', // Subject line
     html: 'Hello world ！     <p> 这是一封用nodejs的nodemailer发送的测试邮件。</p> ' +
-	'<p> </p>'// html body
+	'<p> 示例分享到了github上：<a href=\"https://github.com/luoyjx/nodemailer-demo\">https://github.com/luoyjx/nodemailer-demo</a></p>'// html body
 };
 
 transporter.sendMail(mailOptions, function(error, info){
