b5bc9e3360f17792b1a8b140f0898032f51df54c
yanjixiong <yjk99@qq.com>
2015-02-16 14:53:49 +0800

    nodemailer的示例
    库的版本是0.3.43，和最新的用法有点点不一样，cnodejs用的是这个版本

---
+++ email.js
@@ -0,0 +1,27 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport('SMTP', {
+    host: 'smtp.gaoqixhb.com',
+	secureConnection: true,
+    port: 465,
+    auth: {
+        user: 'system@gaoqixhb.com',
+        pass: '邮箱密码'
+    }
+});
+
+var mailOptions = {
+    from: '搞起博客 <system@gaoqixhb.com>', // 如果不加<xxx@xxx.com> 会报语法错误
+    to: '1111@xx.com,222@xx.com,333@xx.com', // list of receivers
+    subject: '这是一封测试邮件', // Subject line
+    html: 'Hello world ！     <p> 这是一封用nodejs的nodemailer发送的测试邮件。</p> ' +
+	'<p> </p>'// html body
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        console.log(error);
+    }else{
+        console.log('Message sent: ' + info.response);
+    }
+});
